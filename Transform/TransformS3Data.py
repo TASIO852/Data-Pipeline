0970d4f1bab39ca7ce44a784a2c0313aac115dee
Tasio852 <tarsiosantos@outlook.com>
2022-12-18 09:58:07 -0300

    Update

--- Transform/TransformS3Data.py
+++ Transform/TransformS3Data.py
@@ -1,1 +1,11 @@
+# lambda spark emr function 
 import boto3
+from  pyspark.sql  import SparkSession
+
+spark = SparkSession.builder \
+    .appName('transform-s3-data-parquet')\
+    .config('')
+
+
+client = boto3.client('emr')
+
